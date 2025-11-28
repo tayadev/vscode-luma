edab21771a1873efac721607ede6386574649a44
Taya <taya@taya.one>
2025-11-28 22:00:37 +0100

    style: standardize quotes and formatting across configuration files

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as os from 'os';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+
+import * as os from "os";
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+	LanguageClient,
+	type LanguageClientOptions,
+	type ServerOptions,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 function getDefaultServerPath(): string {
 	const platform = os.platform();
-	const executable = platform === 'win32' ? 'luma.exe' : 'luma';
-	return path.join(os.homedir(), '.luma', 'bin', executable);
+	const executable = platform === "win32" ? "luma.exe" : "luma";
+	return path.join(os.homedir(), ".luma", "bin", executable);
 }
 
 function getServerPath(): string {
-	const config = vscode.workspace.getConfiguration('luma');
-	const configuredPath = config.get<string>('serverPath', '');
-	
+	const config = vscode.workspace.getConfiguration("luma");
+	const configuredPath = config.get<string>("serverPath", "");
+
 	if (configuredPath) {
 		return configuredPath;
 	}
-	
+
 	return getDefaultServerPath();
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "luma" is now active!');
@@ -35,30 +39,35 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('luma.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand("luma.helloWorld", () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from luma!');
+		vscode.window.showInformationMessage("Hello World from luma!");
 	});
 
 	context.subscriptions.push(disposable);
 
 	// Initialize the Language Server
 	const serverPath = getServerPath();
-	
+
 	const serverOptions: ServerOptions = {
 		command: serverPath,
-		args: ['lsp'],
-		options: { cwd: vscode.workspace.rootPath || os.homedir() }
+		args: ["lsp"],
+		options: { cwd: vscode.workspace.rootPath || os.homedir() },
 	};
 
 	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'luma' }],
-		outputChannelName: 'Luma Language Server'
+		documentSelector: [{ scheme: "file", language: "luma" }],
+		outputChannelName: "Luma Language Server",
 	};
 
-	client = new LanguageClient('luma', 'Luma Language Server', serverOptions, clientOptions);
-	
+	client = new LanguageClient(
+		"luma",
+		"Luma Language Server",
+		serverOptions,
+		clientOptions,
+	);
+
 	client.start();
 
 	console.log(`Luma LSP server started with path: ${serverPath}`);
